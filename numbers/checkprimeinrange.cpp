24aa8276b7dc2394c85d5e12ea30fd892bedaa45
Suraj182004 <surajyaligar2004@gmail.com>
2025-05-30 12:52:18 +0530

    prime in range

---
+++ numbers/checkprimeinrange.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+bool checkprime(int n){
+    if(n==1)
+        return false;
+    int i=2;
+    for (i = 2; i < sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void PrintPrimesbwrange(int a, int b)
+{
+    for (int i = a; i <= b; i++)
+    {
+        if (checkprime(i))
+        {
+            cout << i << " ";
+        }
+    }
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    PrintPrimesbwrange(a,b);
+    return 0;
+
+}
